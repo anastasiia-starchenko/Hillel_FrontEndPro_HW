c5aff61ef5983271b37461919f12d377a9c7cd0a
anastasiia-starchenko <starchenko.anastasiia.r@gmail.com>
2024-02-08 18:24:59 +0200

    Hillel_FrontEndPro_HW19

--- Homework_19/vite-project/src/components/Navigation/Navigation.jsx
+++ Homework_19/vite-project/src/components/Navigation/Navigation.jsx
@@ -2,37 +2,42 @@
 function Navigation() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="vite-project/src/components/Navigation/Navigation.jsx#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="vite-project/src/components/Navigation/Navigation.jsx#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="vite-project/src/components/Navigation/Navigation.jsx#">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="vite-project/src/components/Navigation/Navigation.jsx#" role="button" data-bs-toggle="dropdown"
-                                   aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="vite-project/src/components/Navigation/Navigation.jsx#">Action</a></li>
-                                    <li><a className="dropdown-item" href="vite-project/src/components/Navigation/Navigation.jsx#">Another action</a></li>
-                                    <li><a className="dropdown-item" href="vite-project/src/components/Navigation/Navigation.jsx#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
+                    <div className="" tabIndex="-1" id="offcanvasNavbar"
+                         aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link</a>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="#" role="button"
+                                       data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <ul className="dropdown-menu">
+                                        <li><a className="dropdown-item" href="#">Action</a></li>
+                                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                                        <li>
+                                            <hr className="dropdown-divider"></hr>
+                                        </li>
+                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <form className="d-flex mt-3" role="search">
+                                <input className="form-control me-2" type="search" placeholder="Search"
+                                       aria-label="Search"></input>
+                                    <button className="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </nav>
