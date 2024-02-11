b970210df291f61563e0b7f31b2e511d38c12d96
anastasiia-starchenko <starchenko.anastasiia.r@gmail.com>
2024-02-11 12:57:34 +0200

    Hillel_FrontEndPro_HW20

--- Homework_20/vite-project/src/assets/helper.js
+++ Homework_20/vite-project/src/assets/helper.js
@@ -2,7 +2,7 @@ const Smiles = ["😀", "😃", "😄", "😁", "😆", "😅"];
 
 export const getMax = (object) => {
     return Object.keys(object).filter(x => {
-        return object[x] == Math.max.apply(null, Object.values(object));
+        return object[x] == Math.max(...Object.values(object));
     });
 };
 
